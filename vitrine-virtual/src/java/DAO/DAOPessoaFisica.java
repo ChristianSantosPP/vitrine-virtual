156c6fb85d3f795a8682724b2fc93f159c93838c
Valeria Ferreira <valeriaffs@hotmail.com>
2017-05-23 23:25:30 -0300

    incluindo os arquivos Dao

---
+++ vitrine-virtual/src/java/DAO/DAOPessoaFisica.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import DAO.DAO;
+
+/**
+ *
+ * @author proto
+ */
+public class DAOPessoaFisica implements DAO {
+
+    @Override
+    public void atualizar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void recuperar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void cadastrar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remover() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
